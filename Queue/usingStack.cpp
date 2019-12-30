4202403b2444e5788171617c0fccaf9a96de94c6
ajaysharma12799 <ajaysharma12799@gmail.com>
2019-12-31 01:30:18 +0530

    updated queue implementation using stack

--- Queue/usingStack.cpp
+++ Queue/usingStack.cpp
@@ -1,152 +1,105 @@
 #include <iostream>
-#define size1 5
-#define size2 5
+
+#define size 5
 
 using namespace std;
 
-class Stack1 {
+class Stack {
+    private:
+        int top;
     public:
-        int top1;
-        int arr1[size1];
+        int arr[size];
 
-        // Constructor to initilize top
-        Stack1() {
-            top1 = -1;
-        }
+    // Constructor to initilize top
+    Stack() {
+        top = -1;
+    }
 
-        // Methods
-        void push1(int data);
-        int pop1();
-};
-
-class Stack2 {
-    public:
-        int top2;
-        int arr2[size1];
-
-        // Constructor to initilize top
-        Stack2() {
-            top2 = -1;
-        }
-
-        // Methods
-        void push2(int data);
-        int pop2();
+    // Methods
+    void push(int data);
+    int pop();
+    bool isEmpty();
 };
 
 class Queue {
     public:
-        Stack1 obj1;
-        Stack2 obj2;
-        int count;
-        void Enqueue(int data);
-        void Dequeue();
-        void display();
+        Stack obj1, obj2;
 
-        Queue() {
-            count = 0;
-        }
+    void Enqueue(int data);
+    int Dequeue();
+    void display();
 };
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char
+    const * argv[]) {
     Queue obj;
     obj.Enqueue(1);
     obj.Enqueue(2);
     obj.Enqueue(3);
     obj.Enqueue(4);
     obj.Enqueue(5);
-
     obj.display();
-
-    obj.Dequeue();
-    obj.Dequeue();
-
-    obj.display();
-
+    cout << "\n Remove Element : " << obj.Dequeue();
+    cout << "\n Remove Element : " << obj.Dequeue();
     obj.Enqueue(10);
-
+    obj.Enqueue(15);
     obj.display();
     return 0;
 }
 
 // Methods of Stack 1
-void Stack1::push1(int data) {
-    if(top1 == size1-1) {
-        cout<<"\n Stack 1 Full";
-    }
-    else {
-        arr1[top1++] = data;
+void Stack::push(int data) {
+    if (top >= size) {
+        cout << "\n Stack is Full";
+    } else {
+        top++;
+        arr[top] = data;
     }
 }
 
-int Stack1::pop1() {
+int Stack::pop() {
     int item;
-    if(top1 == -1) {
-        cout<<"\n Stack 1 Empty";
-    }
-    else {
-        item = arr1[top1--];
-    }
-    return item;
-}
-
-// Methods of Stack 2
-void Stack2::push2(int data) {
-    if(top2 == size2-1) {
-        cout<<"\n Stack 1 Full";
-    }
-    else {
-        arr2[top2++] = data;
+    if (top == -1) {
+        cout << "\n Stack is Empty";
+        return 0;
+    } else {
+        item = arr[top--];
+        return item;
     }
 }
 
-int Stack2::pop2() {
-    int item;
-    if(top2 == -1) {
-        cout<<"\n Stack 1 Empty";
+bool Stack::isEmpty() {
+    if (top == -1) {
+        return true;
+    } else {
+        return false;
     }
-    else {
-        item = arr2[top2--];
-    }
-    return item;
 }
 
 // Method of Queue
 void Queue::Enqueue(int data) {
-    obj1.push1(data);
-    cout<<"\n Count 1 TIme : "<<count;
-    count++;
-    cout<<"\n\t\t Count 2 Time : "<<count;
+    obj1.push(data);
 }
 
-void Queue::Dequeue() {
-    
-    // Stack1 obj1;
-    // Stack2 obj2;
-    int num1, num2, num3;
+int Queue::Dequeue() {
+    int num1, num2;
+    while (!obj1.isEmpty()) {
+        num1 = obj1.pop();
+        obj2.push(num1);
+    }
 
-    if( (obj1.top1 == -1) && (obj2.top2 == -1) ) {
-        cout<<"\n Queue is Empty";
+    num2 = obj2.pop();
+
+    while (!obj2.isEmpty()) { // again poping element from second stack and inserting into first stack
+        num1 = obj2.pop();
+        obj1.push(num1);
     }
-    else {
-        for(int i=0; i<count; i++) {
-            num1 = obj1.pop1();
-            obj2.push2(num1);
-        }
-        num2 = obj2.pop2();
-        cout<<"\n Dequed Element : "<<num2;
-        count--; // Decrementing count after poping element from queue
-        for(int i=0; i<count; i++) { // again poping element from second stack and inserting into first stack
-            num3 = obj2.pop2();
-            obj1.push1(num3);
-        }
-    }
+    return num2;
 }
 
 void Queue::display() {
-    cout<<"\n Queue : ";
-    for(int i=obj1.top1; i>=0; i--) {
-        cout<<"  "<<obj1.arr1[i];
+    cout << "\n Queue : ";
+    for (int i = 0; i < size; i++) {
+        cout << " " << obj1.arr[i];
     }
 }
