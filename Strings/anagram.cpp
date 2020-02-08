40978db008cc2304ec6266fb7f0d6e79b08bfed9
ajaysharma12799 <ajaysharma12799@gmail.com>
2020-02-08 23:55:50 +0530

    added anagram program

--- Strings/anagram.cpp
+++ Strings/anagram.cpp
@@ -1,11 +1,11 @@
-#include <bits/stdc++.h>
 #include <iostream>
 #include <string.h>
-#include <string>
+
+#define MAX_CHAR 256
 
 using namespace std;
 
-bool isanagram(char s1[], char s2[]) {
+bool isanagram(char *s1, char *s2) {
     int length1 = strlen(s1);
     int length2 = strlen(s2);
 
@@ -13,15 +13,27 @@ bool isanagram(char s1[], char s2[]) {
     if(length1 != length2) {
         return false;
     }
-    else {
-        
+
+    int first[MAX_CHAR] = { 0 }, second[MAX_CHAR] = { 0 };
+
+    for(int i=0; s1[i] != '\0' && s2[i] != '\0'; i++) {
+        first[s1[i]]++;
+        second[s2[i]]++;
     }
+
+    // checking condition for int array
+    for(int i=0; i<MAX_CHAR; i++) {
+        if(first[i] != second[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main(int argc, char const *argv[])
 {
-    char str1[] = "Ajay";
-    char str2[] = "Ajay";
+    char str1[] = "ajay";
+    char str2[] = "ajay";
 
     if(isanagram( str1, str2 )) {
         cout<<"\n String is Anagram";
